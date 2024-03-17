6998d777f254d3e0dbf931c980ac464cce36ef8b
lucas vinicius <lucassilved@gmail.com>
2024-03-16 21:23:44 -0300

    feat: adds remote authentication implementation

--- src/authentication/AuthenticationRemote.java
+++ src/authentication/AuthenticationRemote.java
@@ -4,5 +4,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface AuthenticationRemote extends Remote {
-    Boolean login(User user) throws RemoteException;
+    ImplAuthenticationRemote.Credentials login(User user) throws RemoteException;
 }
--- src/authentication/ImplAuthenticationRemote.java
+++ src/authentication/ImplAuthenticationRemote.java
@@ -1,10 +1,23 @@
 package authentication;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImplAuthenticationRemote implements AuthenticationRemote {
-    @Override
-    public Boolean login(User user) throws RemoteException {
-        return null;
+    private final List<User> USERS = new ArrayList<>();
+
+    public ImplAuthenticationRemote() {
+        USERS.add(new User("admin@autohaus.com", "autohaus2022", UserTypes.EMPLOYEE));
+        USERS.add(new User("pedro@mail.com", "flamengo123", UserTypes.CUSTOMER));
+        USERS.add(new User("maria@mail.com", "gatodebotas123", UserTypes.CUSTOMER));
     }
+
+    @Override
+    public Credentials login(User user) throws RemoteException {
+        if (USERS.contains(user)) return new Credentials(Boolean.TRUE, user.type());
+        return new Credentials(Boolean.FALSE, user.type());
+    }
+
+    public record Credentials(Boolean isRegistered, UserTypes useType) {};
 }
