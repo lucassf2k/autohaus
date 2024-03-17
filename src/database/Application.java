9b4b388b6ae227a543a4ade1521c2283ee40f0b6
lucas vinicius <lucassilved@gmail.com>
2024-03-16 22:02:09 -0300

    feat: add entrypoint of database service

---
+++ src/database/Application.java
@@ -0,0 +1,25 @@
+package database;
+
+import authentication.AuthenticationRemote;
+import authentication.ImplAuthenticationRemote;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Application {
+    public static void main(String[] args) {
+        try {
+            final var implAuthenticationRemote = new ImplDatabaseRemote();
+            final var skeleton = (AuthenticationRemote) UnicastRemoteObject
+                    .exportObject(implAuthenticationRemote, 0);
+            LocateRegistry.createRegistry(ImplDatabaseRemote.PORT);
+            final var registry = LocateRegistry.getRegistry(ImplDatabaseRemote.PORT);
+            registry.bind("database", skeleton);
+            System.out.println("serviço de banco de dados rodando em " + ImplDatabaseRemote.PORT);
+        } catch (RemoteException | AlreadyBoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/database/ImplDatabaseRemote.java
+++ src/database/ImplDatabaseRemote.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class ImplDatabaseRemote implements DatabaseRemote {
+    public final static int PORT = 20005;
     private final Map<String, Car> DB = new HashMap<>();
 
 
