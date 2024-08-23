952ea41cedae46833cbcbb8afa8a0e8d326fbe52
lucas vinicius <lucassilved@gmail.com>
2024-08-22 21:15:30 -0300

    feat: adiciona Function<CarCategoris, String> em CarCategories

--- src/shared/CarCategories.java
+++ src/shared/CarCategories.java
@@ -7,11 +7,18 @@ public enum CarCategories implements Serializable {
     ECONOMIC, INTERMEDIATE, EXECUTIVE;
 
     public static String toPTBR(CarCategories carCategory) {
-        return switch (carCategory) {
-            case ECONOMIC -> "Econômico";
-            case INTERMEDIATE -> "Intermediário";
-            case EXECUTIVE -> "Executivo";
+        Function<CarCategories, String> parse = (c) -> {
+            if (c.equals(ECONOMIC)) return "Econômico";
+            if (c.equals(INTERMEDIATE)) return "Intermediário";
+            if (c.equals(EXECUTIVE)) return "Executivo";
+            return "Econômico";
         };
+        return parse.apply(carCategory);
+//        return switch (carCategory) {
+//            case ECONOMIC -> "Econômico";
+//            case INTERMEDIATE -> "Intermediário";
+//            case EXECUTIVE -> "Executivo";
+//        };
     }
 
     public static CarCategories parseToCarCategory(int value) {
